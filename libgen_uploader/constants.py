148451baa662c66b1c9635634659ffdbefd7c193
Francesco Truzzi <francesco@truzzi.me>
2021-03-26 16:49:53 +0100

    version bump

--- libgen_uploader/constants.py
+++ libgen_uploader/constants.py
@@ -1,4 +1,4 @@
-LIBGEN_UPLOADER_VERSION = "0.1.0"
+LIBGEN_UPLOADER_VERSION = "0.1.1"
 
 FICTION_UPLOAD_URL = "https://library.bz/fiction/upload/"
 SCITECH_UPLOAD_URL = "https://library.bz/main/upload/"
